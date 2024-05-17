5eb5cc14b84d3041b817f2caa7dd6471dc3d9b85
erexer <13180883+erexer@users.noreply.github.com>
2024-05-17 12:57:47 -0700

    add emoji extension :)

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,7 +13,10 @@ author = 'Em Rexer, Chris Vernon'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['myst_parser']
+extensions = [
+    'myst_parser',
+    'sphinxemoji.sphinxemoji',
+    ]
 
 templates_path = ['_templates']
 exclude_patterns = []
